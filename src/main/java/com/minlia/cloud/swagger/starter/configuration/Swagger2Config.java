0ecd22fc7994b9d75718b79a2b1275c4b1e1992e
Henry Lin <badcop@163.com>
2018-04-02 17:26:37 +0800

    up

---
+++ src/main/java/com/minlia/cloud/swagger/starter/configuration/Swagger2Config.java
@@ -0,0 +1,75 @@
+package com.minlia.cloud.swagger.starter.configuration;
+
+import com.fasterxml.classmate.TypeResolver;
+import com.minlia.cloud.swagger.properties.SwaggerConfigurationProperties;
+import com.minlia.cloud.swagger.plugins.PageableParameterBuilderPlugin;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.StopWatch;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.TypeNameExtractor;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.UiConfiguration;
+
+/**
+ * Created by user on 11/14/15.
+ */
+@Slf4j
+public class Swagger2Config {
+
+  @Autowired
+  private TypeResolver typeResolver;
+
+  @Autowired
+  private SwaggerConfigurationProperties minliaProperties;
+
+  @Bean
+  public Docket documentation() {
+    log.debug("Starting Swagger");
+    StopWatch watch = new StopWatch();
+    watch.start();
+
+    Docket docket= new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.regex(minliaProperties.getPath())).build().directModelSubstitute(LocalDate.class, java.sql.Date.class)
+      .directModelSubstitute(LocalDateTime.class, java.util.Date.class).pathMapping("/").apiInfo(metadata())
+      ;
+
+    watch.stop();
+    log.debug("Started Swagger in {} ms", watch.getTotalTimeMillis());
+    return docket;
+  }
+
+  @Bean
+  SecurityConfiguration security() {
+    return new SecurityConfiguration(null, null, null, null, null, ApiKeyVehicle.HEADER, "X-Auth-Token", ",");
+  }
+
+  @Bean
+//  @ConditionalOnClass(value = {Pageable.class})
+  PageableParameterBuilderPlugin pageableParameterBuilderPlugin(TypeNameExtractor nameExtractor, TypeResolver resolver) {
+    return new PageableParameterBuilderPlugin(nameExtractor, resolver);
+  }
+
+
+
+  @Bean
+  public UiConfiguration uiConfig() {
+    return UiConfiguration.DEFAULT;
+  }
+
+  private ApiInfo metadata() {
+//        return new ApiInfoBuilder().title("Minlia Cloud Development Environment System API").description("Minlia Cloud Development Environment System API").version("2.0").contact("cloud@minlia.com").build();
+//        return new ApiInfoBuilder().title(minliaProperties.getSwagger().getTitle()).description(minliaProperties.getSwagger().getDescription()).version(minliaProperties.getSwagger().getVersion()).contact(minliaProperties.getSwagger().getContact()).build();
+    return new ApiInfoBuilder().title(minliaProperties.getTitle()).description(minliaProperties.getDescription()).version(minliaProperties.getVersion()).contact(minliaProperties.getContact()).build();
+  }
+
+
+}
---
+++ src/main/java/com/minlia/cloud/swagger/starter/configuration/SwaggerAutoConfiguration.java
@@ -0,0 +1,42 @@
+package com.minlia.cloud.swagger.starter.configuration;
+
+import com.minlia.cloud.swagger.EnableDevSwagger;
+import com.minlia.cloud.swagger.properties.SwaggerConfigurationProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ConditionalOnClass(Swagger2Config.class)
+@EnableConfigurationProperties(SwaggerConfigurationProperties.class)
+@Profile(value = {"!production"})
+@ConditionalOnProperty(prefix = "system.development.swagger", name = "enabled", havingValue = "true")
+public class SwaggerAutoConfiguration {
+
+  @Configuration
+  @EnableDevSwagger
+  @ConditionalOnMissingBean(EnableDevSwagger.class)
+  public static class Swagger2AutoConfig {
+  }
+
+
+  @Configuration
+  @EnableWebMvc
+  public static class EnableMinliaStaticResourceConfiguration extends WebMvcConfigurerAdapter {
+    private  final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+      registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+  }
+
+
+}
